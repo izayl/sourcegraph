8fe7b7160fb5edfbbc010868d244d33fc90ee7ac
Camden Cheek <camden@ccheek.com>
2021-07-20 09:54:04 -0600

    rename File to Content in stream events
    This commit renames our stream events to use the term "content" in the
    places that it would use "file". This does not yet modify our result
    types with the same convention, but that will come as a followup change.
    The intent is to make the different events better match up with our
    result types, so now we have Content, Symbol, and Path events that used
    to be represented by the catch-all File event.

--- client/shared/src/components/FileMatch.tsx
+++ client/shared/src/components/FileMatch.tsx
@@ -4,14 +4,7 @@ import React, { useEffect, useState } from 'react'
 import { Observable } from 'rxjs'
 import { AggregableBadge, Badge } from 'sourcegraph'
 
-import {
-    FileLineMatch,
-    FileSymbolMatch,
-    FilePathMatch,
-    getFileMatchUrl,
-    getRepositoryUrl,
-    getRevision,
-} from '../search/stream'
+import { ContentMatch, SymbolMatch, PathMatch, getFileMatchUrl, getRepositoryUrl, getRevision } from '../search/stream'
 import { SettingsCascadeProps } from '../settings/settings'
 import { pluralize } from '../util/strings'
 import { useRedesignToggle } from '../util/useRedesignToggle'
@@ -44,7 +37,7 @@ interface Props extends SettingsCascadeProps {
     /**
      * The file match search result.
      */
-    result: FileLineMatch | FileSymbolMatch | FilePathMatch
+    result: ContentMatch | SymbolMatch | PathMatch
 
     /**
      * Formatted repository name to be displayed in repository link. If not
@@ -92,7 +85,7 @@ export const FileMatch: React.FunctionComponent<Props> = props => {
 
     const result = props.result
     const items: MatchItem[] =
-        result.type === 'file'
+        result.type === 'content'
             ? result.lineMatches.map(match => ({
                   highlightRanges: match.offsetAndLengths.map(([start, highlightLength]) => ({
                       start,
